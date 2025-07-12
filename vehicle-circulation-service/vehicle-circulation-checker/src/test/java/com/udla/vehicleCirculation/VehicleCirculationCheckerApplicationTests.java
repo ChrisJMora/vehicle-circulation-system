74fbd598702605fff130f63419a275c4baf42937
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-12 10:08:19 -0500

    Se inicializa el proyecto

---
+++ vehicle-circulation-service/vehicle-circulation-checker/src/test/java/com/udla/vehicleCirculation/VehicleCirculationCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udla.vehicleCirculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleCirculationCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
