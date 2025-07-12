417cc8bc2f54e1102cd19a71bd6c82c93c82de88
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-12 11:08:08 -0500

    Se crea el componente circulation-chek y el servicio para la API

---
+++ vehicle-circulation-app/src/app/circulation-check/circulation-check.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circulation-check',
+  imports: [],
+  templateUrl: './circulation-check.component.html',
+  styleUrl: './circulation-check.component.css'
+})
+export class CirculationCheckComponent {
+
+}
