0f27b7eb6679a7c50d6a1cee8b92795d1ca11966
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-12 12:13:00 -0500

    Se crea el formulario del frontend

--- vehicle-circulation-app/src/app/circulation-check/circulation-check.component.ts
+++ vehicle-circulation-app/src/app/circulation-check/circulation-check.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CirculationService, VehicleCirculationRequest, VehicleCirculationResponse } from '../services/circulation.service';
 
 @Component({
   selector: 'app-circulation-check',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './circulation-check.component.html',
-  styleUrl: './circulation-check.component.css'
 })
 export class CirculationCheckComponent {
+  request: VehicleCirculationRequest = {
+    licensePlate: '',
+    date: '',
+    time: ''
+  };
 
+  response?: VehicleCirculationResponse;
+  error?: string;
+
+  constructor(private circulationService: CirculationService) {}
+
+  onSubmit() {
+    this.circulationService.checkCirculation(this.request).subscribe({
+      next: (res) => {
+        this.response = res;
+        this.error = undefined;
+      },
+      error: () => {
+        this.error = 'Error al consultar el backend.';
+        this.response = undefined;
+      }
+    });
+  }
 }
